b59fdeb9ad45651f281659ba1b2d22b05ab3787e
lsim96 <leon.simonoskiii@gmail.com>
2025-03-03 14:10:46 +0100

    Class02_Changes

--- Class02-built_in_modules/code/users_manager/usersService.js
+++ Class02-built_in_modules/code/users_manager/usersService.js
@@ -45,4 +45,48 @@ const newUser = {
   email: "bob@email.com",
 };
 
-addUser(newUser);
+// addUser(newUser);
+
+const editUser = (id, data) => {
+  const existingUsers = fs.readFileSync("users.json", "utf-8");
+  const parsedUsers = JSON.parse(existingUsers);
+  const user = parsedUsers.find((user) => user.id === id);
+  user.name = data.name;
+  user.username = data.username;
+  user.email = data.email;
+
+  fs.writeFileSync("users.json", JSON.stringify(parsedUsers), null, 2);
+};
+
+const updatedUser = {
+  name: "Leon Simonoski",
+  username: "leonsim",
+  email: "leonsimonoski@gmail.com",
+};
+
+// editUser(9, updatedUser);
+
+const deleteUser = (id) => {
+  const existingUsers = fs.readFileSync("users.json", "utf-8");
+  const parsedUsers = JSON.parse(existingUsers);
+  const removedUser = parsedUsers.filter((user) => user.id !== id);
+
+  fs.writeFileSync("users.json", JSON.stringify(removedUser), null, 2);
+};
+
+// deleteUser(9);
+
+const deleteAll = () => {
+  const existingUsers = fs.readFileSync("users.json", "utf-8");
+  const parsedUsers = JSON.parse(existingUsers);
+  parsedUsers.length = 0;
+  // parsedUsers.splice(0, parsedUsers.length);
+  // Trying different ways ovah here!!
+
+  fs.writeFileSync("users.json", JSON.stringify(parsedUsers), null, 2);
+
+  // fs.writeFileSync("users.json", "", null, 2);
+  // Here as well!
+};
+
+deleteAll();
